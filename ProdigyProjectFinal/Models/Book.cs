a21ef6706ef3ee6e8b893de203f68579eed7946a
User <User@LAB3-3>
2024-02-22 12:14:15 +0200

    isbn implementation

--- ProdigyProjectFinal/Models/Book.cs
+++ ProdigyProjectFinal/Models/Book.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//using Windows.Devices.Bluetooth.Advertisement;
 
 namespace ProdigyProjectFinal.Models
 {
@@ -12,13 +13,18 @@ namespace ProdigyProjectFinal.Models
         public string AuthorKey { get; set; }
         public string AuthorName { get; set; }
         public string Title { get; set; }
+        public string ISBN { get; set; }
 
-        public Book(string publisher, string authorKey, string authorName, string title)
+        public string ISBNstring { get; set; }
+
+        public Book(string publisher, string authorKey, string authorName, string title, string iSBN)
         {
             Publisher = publisher;
             AuthorKey = authorKey;
             AuthorName = authorName;
             Title = title;
+            ISBN = iSBN;
+            ISBNstring = $"https://covers.openlibrary.org/b/isbn/{ISBN}-S.jpg";
         }
     }
 }
